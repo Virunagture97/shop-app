6701adbcc6f556a6cfbae388bab2f1018b6db6e5
Virendra Nagture <107663177+Virunagture97@users.noreply.github.com>
2024-04-28 12:33:12 +0530

    Update ProductDetails.js

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -18,7 +18,7 @@ function ProductDetails(props) {
     <div className="ui grid container">
       <div className="ui placeholder segment">
         <div className="ui two column stackable center aligned grid">
-          <div className="ui vertical divider">AND</div>
+          <div className="ui vertical divider"></div>
           <div className="middle aligned row">
             <div className="column lp">
               <img className="ui fluid image" src={image} />
